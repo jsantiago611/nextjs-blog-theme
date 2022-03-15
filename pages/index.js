d4968fdfbe264a2822b14cf4017b2601f23e37be
Jonathan Santiago <36797222+jsantiago611@users.noreply.github.com>
2022-03-15 14:35:52 -0400

    updated blog description field size and font fam

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Index({ posts, globalData }) {
       <SEO title={globalData.name} description={globalData.blogTitle} />
       <Header name={globalData.name} />
       <main className="w-full">
-        <h1 className="text-3xl lg:text-5xl text-center mb-12">
+        <h1 className="text-2xl lg:text-4xl text-center mb-12" style="font-family: var(--font-secondary);">
           {globalData.blogTitle}
         </h1>
         <ul className="w-full">
