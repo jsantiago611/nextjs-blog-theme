4fcca76b71160f9d29b94716e685c0576af80c06
Jonathan Santiago <36797222+jsantiago611@users.noreply.github.com>
2022-03-15 14:43:38 -0400

    descript from h1 to p

--- pages/index.js
+++ pages/index.js
@@ -14,9 +14,9 @@ export default function Index({ posts, globalData }) {
       <SEO title={globalData.name} description={globalData.blogTitle} />
       <Header name={globalData.name} />
       <main className="w-full">
-        <h1 className="text-2xl lg:text-4xl text-center mb-12">
+        <p className="text-2xl lg:text-4xl text-center mb-12">
           {globalData.blogTitle}
-        </h1>
+        </p>
         <ul className="w-full">
           {posts.map((post) => (
             <li
